d9158f419c87a4c18642a28d27c641c2e7a60702
AurelienAllenic <aurelien.allenic@gmail.com>
2025-07-03 12:22:58 +0200

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "storage": {
+    "userTable2": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
